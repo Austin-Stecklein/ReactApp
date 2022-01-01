47bd356b2be14be8c428922a2e3380e9d6d2aea7
Austin-Stecklein <ste18088@byui.edu>
2022-01-01 15:42:48 -0700

    Base
    Co-authored-by: Austin Talbot <austintalbot@gmail.com>

---
+++ test-app/src/Components/SideBar.js
@@ -0,0 +1,18 @@
+import Styled-components
+
+const SideBar = ()=>{
+  return 
+  <Wrapper>
+  <LogoContainer></LogoContainer>
+  <SidebarIcons></SidebarIcons>
+  <ProfileIcon></ProfileIcon>
+  </Wrapper>
+}
+
+export default SideBar
+
+const wrapper = Styled.div`
+  height:calc(100vh-100px);
+  padding:50px;
+  width:
+`
